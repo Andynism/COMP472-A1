e0088319968c9a20b74a9ae4004b894a8d620139
Felix Lapierre <felix.lapierre1@gmail.com>
2020-10-19 17:23:59 -0400

    Updated the other 4 models with the changes to metrics and confusion matrix apis

--- src/perceptron.py
+++ src/perceptron.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import Perceptron
 from common import confusion_matrix
 from common import fileutils
 from common import metrics
+from common import alphabet
 
 def predict_perceptron(input_data, validation_data, test_data, filename):
     inputX = input_data.iloc[:,0:1024] # Binary features (first 1024 columns)
@@ -41,8 +42,8 @@ test1 = fileutils.load_csv("test_with_label_1")
 
 test_predictions1, test_correct1 = predict_perceptron(input1, valid1, test1, "Perceptron-DS1")
 
-confusion_matrix.create_alphabet(test_predictions1, test_correct1)
-metrics.compute(test_predictions1, test_correct1, 26)
+confusion_matrix.create(test_predictions1, test_correct1, alphabet.latin(), "Perceptron-DS1")
+metrics.compute(test_predictions1, test_correct1, alphabet.latin(), "Perceptron-DS1")
 
 print("===Perceptron Data Set 2===")
 input2 = fileutils.load_csv("train_2")
@@ -51,5 +52,5 @@ test2 = fileutils.load_csv("test_with_label_2")
 
 test_predictions2, test_correct2 = predict_perceptron(input2, valid2, test2, "Perceptron-DS2")
 
-confusion_matrix.create_greek(test_predictions2, test_correct2)
-metrics.compute(test_predictions2, test_correct2, 10)
+confusion_matrix.create(test_predictions2, test_correct2, alphabet.greek(), "Perceptron-DS2")
+metrics.compute(test_predictions2, test_correct2, alphabet.greek_no_unicode(), "Perceptron-DS2")
